87a9bf3af7ca6a9e034352a3cf373643959821df
trungdongle <ngu0ic0dedao1@gmail.com>
2025-08-23 14:58:12 +0700

    update typescript

--- smartfact-frontend/src/app/page.tsx
+++ smartfact-frontend/src/app/page.tsx
@@ -16,15 +16,12 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
+              src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
-          </li>
-          <li className="tracking-[-.01em]">
-            Dai.
           </li>
         </ol>
 
